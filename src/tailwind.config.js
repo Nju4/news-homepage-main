741f13b322f3dcbcd13ea9a09c15ffc55a142fea
Najzua Ramdhanni <149668669+Nju4@users.noreply.github.com>
2024-10-03 22:54:01 +0700

    Add files via upload

---
+++ src/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ['./**/*.html'],
+  theme: {
+    extend: {
+      colors: {
+        'cu-yellow': 'hsl(35, 77%, 62%)',
+        'dark-blue': 'hsl(240, 100%, 5%)',
+        'grayish-blue': 'hsl(236, 13%, 42%)',
+        'orange-red' : 'hsl(5, 85%, 63%)'
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
